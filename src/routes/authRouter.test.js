b33a775ea220bdb67fc532af85478b2565374ec5
James Finlinson <the30clues@gmail.com>
2024-10-01 02:11:54 -0600

    Prepare for AuthRouter testing

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,19 +1,25 @@
 const request = require('supertest');
 const app = require('../service');
+const { randomName } = require('./test-helper');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
 
 beforeAll(async () => {
-  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  testUser.email = randomName() + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
   expectValidJwt(testUserAuthToken);
 });
 
+afterAll(async () => {
+  expect(testUserAuthToken).toBeTruthy();
+  await request(app).delete("/api/auth").set('Authorization', `Bearer ${testUserAuthToken}`)
+});
+
 test('login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
-  expect(loginRes.status).toBe(200);
+  expectSuccessfulResponse(loginRes);
   expectValidJwt(loginRes.body.token);
 
   const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
@@ -21,6 +27,27 @@ test('login', async () => {
   expect(password).toBeTruthy(); // Use the variable to pass the linter
 });
 
+test('register', async () => {
+  // Response should be successful
+  // New user should have a different ID than our original user
+  // New user should have required fields
+  // New user should have a valid Jwt
+});
+
+test('update', async () => {
+  // Response should be successful
+  // Response should contain the updated information
+});
+
+test('delete', async () => {
+  // Create new user
+  // Delete new user
+  // Expect subsequent write to fail
+});
+
+function expectSuccessfulResponse(res) {
+  expect(res.status).toBe(200);
+}
 function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 }
