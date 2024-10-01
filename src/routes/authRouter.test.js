c6890184de6937a42b7263302daf25dec4c3e3e7
James Finlinson <the30clues@gmail.com>
2024-10-01 02:49:26 -0600

    Include AuthRouter tests --> 54% coverage

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -2,39 +2,76 @@ const request = require('supertest');
 const app = require('../service');
 const { randomName } = require('./test-helper');
 
+const API_ROOT = '/api/auth';
+
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
+let testUserServerResult;
 
 beforeAll(async () => {
-  testUser.email = randomName() + '@test.com';
-  const registerRes = await request(app).post('/api/auth').send(testUser);
-  testUserAuthToken = registerRes.body.token;
-  expectValidJwt(testUserAuthToken);
+  testUserServerResult =  await registerNewUser();
+  testUserAuthToken = testUserServerResult.token;
 });
 
 afterAll(async () => {
   expect(testUserAuthToken).toBeTruthy();
-  await request(app).delete("/api/auth").set('Authorization', `Bearer ${testUserAuthToken}`)
+  await request(app).delete("/api/auth").set('Authorization', `Bearer ${testUserAuthToken}`);
+  // TODO: Delete the user from the database
 });
 
-test('login', async () => {
-  const loginRes = await request(app).put('/api/auth').send(testUser);
+async function registerNewUser() {
+  testUser.email = randomName() + '@test.com';
+  const registerRes = await request(app).post(API_ROOT).send(testUser);
+  expectSuccessfulResponse(registerRes);
+  expectValidJwt(registerRes.body.token);
+  return registerRes.body;
+}
+
+async function loginTestUser() {
+  const loginRes = await request(app).put(API_ROOT).send(testUser);
   expectSuccessfulResponse(loginRes);
   expectValidJwt(loginRes.body.token);
+  return loginRes;
+}
 
+test('login', async () => {
+  const loginRes = await loginTestUser();
   const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
   expect(loginRes.body.user).toMatchObject(user);
   expect(password).toBeTruthy(); // Use the variable to pass the linter
 });
 
-test('register', async () => {
-  // Response should be successful
-  // New user should have a different ID than our original user
-  // New user should have required fields
-  // New user should have a valid Jwt
+test('register successfully', async () => {
+  const newUser = await registerNewUser();
+  expect(newUser.user.id).not.toBe(testUserServerResult.user.id);
+
+  expect(newUser).toHaveProperty("user.id");
+  expect(newUser).toHaveProperty("user.name");
+  expect(newUser).toHaveProperty("user.email");
+  expect(newUser).toHaveProperty("user.roles", [{ role: 'diner' }]);
 });
 
-test('update', async () => {
+test('register without password', async () => {
+  const registerRequest = {name: "Test user", email: "missing_pwd@test.com"};
+  const registerRes = await request(app).post(API_ROOT).send(registerRequest);
+  expect(registerRes.statusCode).toBe(400);
+  expect(registerRes.text).toContain("required");
+});
+
+test('update successfully', async () => {
+  const newUser = await registerNewUser();
+
+  const updatedUser = {email: randomName() + "@test.com", password: "newPassword"};
+  const updateRes = await request(app).put(API_ROOT+"/"+newUser.user.id).set('Authorization', `Bearer ${newUser.token}`).send(updatedUser);
+  expectSuccessfulResponse(updateRes);
+  expect(updateRes.body.email).toBe(updatedUser.email);
+})
+
+test('update invalid user', async () => {
+  const updatedUser = {email: "bademail@test.com", password: "badpassword" };
+  const updateRes = await request(app).put(API_ROOT+"/invalid-user-id").set('Authorization', `Bearer ${testUserAuthToken}`).send(updatedUser);
+  expect(updateRes.statusCode).toBe(403);
+  expect(updateRes.text).toContain("unauthorized");
   // Response should be successful
   // Response should contain the updated information
 });
