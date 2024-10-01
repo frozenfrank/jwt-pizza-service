3c861356c217c7db489d27224166bb1c04fbea37
James Finlinson <the30clues@gmail.com>
2024-10-01 01:52:23 -0600

    Introduce test-helper.js

---
+++ src/routes/test-helper.js
@@ -0,0 +1,24 @@
+const { Role, DB } = require('../database/database.js');
+
+if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+  console.log("Automatically increasing JEST timeout for debugging purposes");
+  jest.setTimeout(60 * 1000 * 5); // 5 minutes
+}
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  await DB.addUser(user);
+
+  user.password = 'toomanysecrets';
+  return user;
+}
+
+module.exports.randomName = randomName;
+module.exports.createAdminUser = createAdminUser;
