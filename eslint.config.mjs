c72e96bc39855b7f17645d8bc477739f80bfa490
StephenKMorgan <stephen.morgan.dhi@gmail.com>
2024-11-18 12:18:22 -0700

    feat: update ESLint configuration for module support and load tests
    (cherry picked from commit 2b9bec5096903df59136fbe66e9ae608865a23ea)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,40 @@ import pluginJs from "@eslint/js";
 
 
 export default [
-  { files: ["**/*.js"], languageOptions: {sourceType: "commonjs"} },
-  { languageOptions: { globals: globals.node } },
-  { languageOptions: { globals: globals.jest } },
-  { ignores: ["loadTests/"] },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        k6: 'readonly',
+        __VU: 'readonly',
+        __ITER: 'readonly'
+      }
+    }
+  },
   pluginJs.configs.recommended,
+  {
+    files: ['loadTests/**/*.js'],
+    rules: {
+      'import/no-unresolved': 'off',
+      'no-unused-expressions': 'off'
+    }
+  },
+  {
+    files: ['**/*.test.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+        describe: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        jest: 'readonly'
+      }
+    }
+  }
 ];
