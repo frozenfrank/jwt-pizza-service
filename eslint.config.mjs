ff080e63f6592a4803c8bb76c2a4a6711ca1bb7e
James Finlinson <the30clues@gmail.com>
2024-11-29 17:37:21 -0700

    Exclude load tests from linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,5 +6,6 @@ export default [
   { files: ["**/*.js"], languageOptions: {sourceType: "commonjs"} },
   { languageOptions: { globals: globals.node } },
   { languageOptions: { globals: globals.jest } },
+  { ignores: ["loadTests/"] },
   pluginJs.configs.recommended,
 ];
