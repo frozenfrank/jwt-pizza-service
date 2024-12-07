d9495b0a31d27771df983c460d282531aaf070b8
James Finlinson <the30clues@gmail.com>
2024-12-06 23:15:06 -0700

    Enable auth token expiration

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -166,7 +166,7 @@ authRouter.put(
 
 async function setAuth(user) {
   const tokenPayload = {...user, token_salt: Math.random(), timestamp: +new Date};
-  const token = jwt.sign(tokenPayload, config.jwtSecret);
+  const token = jwt.sign(tokenPayload, config.jwtSecret, { expiresIn: "24h" });
   await DB.loginUser(user.id, token);
   return token;
 }
