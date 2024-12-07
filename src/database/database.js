8ef63c424c5121843295eb098ff43c0b163dca16
James Finlinson <the30clues@gmail.com>
2024-12-06 21:37:12 -0700

    Remove slow DB calls to validate to validate each menu item...
    Instead, we rely on the order validation already placed in the client
    which does it much more efficiently. Additionally, this allows us to
    retain a log of the orders submitted as opposed to crashing with an
    invalid item request.

--- src/database/database.js
+++ src/database/database.js
@@ -173,8 +173,7 @@ class DB {
       const orderResult = await this.query(connection, `INSERT INTO dinerOrder (dinerId, franchiseId, storeId, date) VALUES (?, ?, ?, now())`, [user.id, order.franchiseId, order.storeId]);
       const orderId = orderResult.insertId;
       for (const item of order.items) {
-        const menuId = await this.getID(connection, 'id', item.menuId, 'menu');
-        await this.query(connection, `INSERT INTO orderItem (orderId, menuId, description, price) VALUES (?, ?, ?, ?)`, [orderId, menuId, item.description, item.price]);
+        await this.query(connection, `INSERT INTO orderItem (orderId, menuId, description, price) VALUES (?, ?, ?, ?)`, [orderId, item.menuId, item.description, item.price]);
       }
       return { ...order, id: orderId };
     } finally {
